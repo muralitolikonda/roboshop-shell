43ec397583b2b549ee5eb4af9966d00bfc7a8b47
murali tolikonda <mtolikonda03@gmail.com>
2024-04-11 10:20:18 +0100

    creating mongodb script

---
+++ catalogue.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at:$TIMESTAMP" &>> $LOGFILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e  "$2 ... $R FAILED $N "
+    else
+        echo -e  "$2 ... $G SUCCESS $N "
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R ERROR $N:: Please run the script with the root user $N"
+    exit 1
+else 
+    echo "You are root user"
+fi
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing git" 
