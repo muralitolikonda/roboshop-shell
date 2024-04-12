80086430df6746a12dd38f660fdd8a8cd79c549d
murali tolikonda <mtolikonda03@gmail.com>
2024-04-12 10:22:36 +0100

    creating mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Enabling mongoDB"
 systemctl start mongod  &>> $LOGFILE
 VALIDATE $? "Starting mongoDB"
 
-sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf  &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf  &>> $LOGFILE
 VALIDATE $? "Remote access to mongoDB"
 
 systemctl restart mongod  &>> $LOGFILE
---
+++ redis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+exec &> $LOGFILE
+
+echo "script started executing at:$TIMESTAMP" &>> $LOGFILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e  "$2 ... $R FAILED $N "
+    else
+        echo -e  "$2 ... $G SUCCESS $N "
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R ERROR $N:: Please run the script with the root user $N"
+    exit 1
+else 
+    echo "You are root user"
+fi
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+dnf module enable redis:remi-6.2 -y
+
+dnf install redis -y
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+
+systemctl enable redis
+
+systemctl start redis
