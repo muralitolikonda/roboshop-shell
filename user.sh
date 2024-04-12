496fea3ae39975161ddfa506e5648cc83f9adc63
murali tolikonda <mtolikonda03@gmail.com>
2024-04-12 12:09:05 +0100

    roboshop-shell

--- user.sh
+++ user.sh
@@ -38,10 +38,17 @@ VALIDATE $? "enabling nodejs:18"
 dnf install nodejs -y  &>> $LOGFILE
 VALIDATE $? "installing nodejs"
 
-useradd roboshop  &>> $LOGFILE
-VALIDATE $? "creating roboshop user"
+id roboshop
 
-mkdir /app
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "creating roboshop user"
+else 
+    echo -e "roboshop user creation...$Y SKIPPING $N "
+fi
+
+mkdir -p /app
 
 curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
 VALIDATE $? "downloading the zip file"
